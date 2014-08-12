d244b0f5891a26f75d5e4ed9d7b0c022e66be99e
Kevin P. Dyer <kpdyer@gmail.com>
2014-08-12 14:38:30 +0100

    update to parse multi-message TCP streams

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ pypcapparser
 ============
 
 a pcap-parsing library
+
+### Example Usage
+
+```python
+import pypcapparser
+
+extracted_streams = pypcapparser.process_pcap(filename="test.pcap",
+                                              protocols=[pypcapparser.TCP],
+                                              dports=pypcapparser.HTTP_PORTS)
+
+for stream in extracted_streams:
+    print stream
+```
--- pypcapparser/__init__.py
+++ pypcapparser/__init__.py
@@ -2,6 +2,8 @@
 
 import nids
 
+from http_parser.parser import HttpParser
+
 TCP = "tcp"
 HTTP_PORTS = [80, 8080]
 
@@ -11,6 +13,50 @@ streams_ = []
 dports_ = []
 session_start_ = -1
 session_end_ = -1
+
+def parse_without_content_length(http_stream):
+    http_message = http_stream.partition('\r\n\r\n')[0] + '\r\n\r\n'
+    http_stream = http_stream.partition('\r\n\r\n')[2]
+    return (http_message, http_stream)
+
+def parse_with_content_length(http_stream):
+    p = HttpParser()
+    p.execute(http_stream, len(http_stream))
+    headers = p.get_headers()
+    body = p.recv_body()
+
+    if 'CONTENT-LENGTH' in headers:
+        body_length = int(headers['CONTENT-LENGTH'])
+    else:
+        body_length = len(body)
+
+    http_message_headers = http_stream.partition('\r\n\r\n')[0] + '\r\n\r\n'
+    http_message_body = body[:body_length]
+    http_message = http_message_headers + http_message_body
+    http_stream = body[body_length:]
+
+    return (http_message, http_stream)
+
+def is_content_length_in_message(http_stream):
+    p = HttpParser()
+    p.execute(http_stream, len(http_stream))
+    headers = p.get_headers()
+    return ('CONTENT-LENGTH' in headers)
+
+
+def get_first_message(http_stream):
+    content_length_in_first_message = is_content_length_in_message(http_stream)
+    if content_length_in_first_message:
+        return parse_with_content_length(http_stream)
+    else:
+        return parse_without_content_length(http_stream)
+
+def http_stream_to_array(http_stream):
+    retval = []
+    while http_stream != '':
+        (http_message, http_stream) = get_first_message(http_stream)
+        retval.append(http_message)
+    return retval
 
 def handleTcpStream(tcp):
     global streams_, session_start_, session_end_
@@ -27,8 +73,10 @@ def handleTcpStream(tcp):
     	tcp.discard(0)
     elif tcp.nids_state in end_states:
         session_end_ = nids.get_pkt_ts()
-        streams_.append({'clientServerMessages': [tcp.server.data[:tcp.server.count]],
-                         'serverClientMessages': [tcp.client.data[:tcp.client.count]],
+        clientServerMessages = http_stream_to_array(tcp.server.data[:tcp.server.count])
+        serverClientMessages = http_stream_to_array(tcp.client.data[:tcp.client.count])
+        streams_.append({'clientServerMessages': clientServerMessages,
+                         'serverClientMessages': serverClientMessages,
                          'sessionStart': session_start_,
                          'sessionEnd': session_end_})
 
